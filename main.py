0ee06bc417a31dbfcc8546c9ce4ffc178605260b
Mimblaa <80205707+Mimblaa@users.noreply.github.com>
2023-04-14 22:26:17 +0200

    text class update

--- main.py
+++ main.py
@@ -76,6 +76,37 @@ class Background:
         surface.blit(self.image, self.rect)
 
 
+class Text:
+    def __init__(self, font_size=30):
+        self.font = pygame.font.SysFont(None, font_size)
+        self.text = ' '
+        self.color = (255, 255, 255)
+        self.rect = None
+
+    def set_text(self, text):
+        self.text = text
+        self.render()
+
+    def set_color(self, color):
+        self.color = color
+        self.render()
+
+    def render(self):
+        self.rect = self.font.render(self.text, True, self.color)
+
+    def draw_on(self, surface):
+        if self.rect:
+            surface.blit(self.rect, (0, 0))
+
+    def show_message(self, message, board, duration=30):
+        self.set_text(message)
+        self.render()
+        # Add a delay to show the message for `duration` seconds
+        time.sleep(duration)
+        # Clear the text to remove it from the screen
+        self.set_text('')
+
+
 class Timer:
     def __init__(self, game_time=10):
         self.clock = pygame.time.Clock()
@@ -116,6 +147,7 @@ class Game():
         self.hero2 = Hero(width=20, height=20, x=width//3,
                           y=height//3, color=(255, 0, 0))
         self.timer = Timer()
+        self.text = Text()
         self.tab = []
 
     def run(self):
@@ -123,6 +155,7 @@ class Game():
         threads = [
             th.Thread(target=self.timer.count_down),
             th.Thread(target=self.spawn_items),
+            th.Thread(target=self.text.show_message, args=("Hello, world!",self.board)),
         ]
 
         # Run in separate threads
@@ -134,6 +167,7 @@ class Game():
                 self.background,
                 self.hero1,
                 self.hero2,
+                self.text,
                 *self.tab,
             )
 
