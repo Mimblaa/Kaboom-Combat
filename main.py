99d7397807532228fc50d99287cd70fe5626526d
Mimblaa <80205707+Mimblaa@users.noreply.github.com>
2023-05-09 21:33:56 +0200

    add Heart and Bomb

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ class Board:
         for arg in args:
             if isinstance(arg, pygame.sprite.Group):
                 arg.draw(self.surface)
+            elif isinstance(arg, Hero):
+                arg.draw_on(self.surface)
+                for heart in arg.hearts:
+                    heart.draw_on(self.surface)
             else:
                 arg.draw_on(self.surface)
 
@@ -43,11 +47,26 @@ class Drawable:
 
 
 class Hero(Drawable):
-    def __init__(self, width, height, x, y, color=(0, 255, 0)):
+    def __init__(self, width, height, x, y, color=(0, 255, 0), lives=3, name="Player"):
         super().__init__(width, height, x, y, color)
         self.surface.fill(color)
         self.width = width
         self.height = height
+        self.lives = lives
+        self.name = name
+        self.hearts = []
+
+    def set_hearts(self, hearts):
+        self.hearts = hearts
+        self.update_hearts()
+
+    def update_hearts(self):
+        for i, heart in enumerate(self.hearts):
+            if i < self.lives:
+                heart.live_type = True
+            else:
+                heart.live_type = False
+            heart.update_image()
 
     def move(self, x, y, board):
 
@@ -61,6 +80,40 @@ class Hero(Drawable):
 
         self.rect.x += x
         self.rect.y += y
+
+    def remove_live(self):
+        if self.lives > 1:
+            self.lives -= 1
+        elif self.lives == 1:
+            self.lives -= 1
+            print("Player " + self.name + " is dead")
+        self.update_hearts()
+
+
+class Heart:
+    def __init__(self, width, height, live_type, number, player):
+        self.width = width
+        self.height = height
+        self.x_pos = width * 0.09 + (number - 1) * width * 0.029
+        self.y_pos = height * 0.52 + (player - 1) * height * 0.308
+        self.live_type = live_type
+        self.update_image()
+
+    def update_image(self):
+        if self.live_type:
+            image_file = 'images/full_heart.png'
+        else:
+            image_file = 'images/dead_heart.png'
+
+        self.image = pygame.image.load(image_file)
+        self.image = pygame.transform.scale(
+            self.image, (int(self.width * 0.023), int(self.height * 0.035)))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x_pos
+        self.rect.y = self.y_pos
+
+    def draw_on(self, surface):
+        surface.blit(self.image, self.rect)
 
 
 class Background:
@@ -77,21 +130,29 @@ class Background:
 
 
 class Timer():
-    def __init__(self, game_time=10):
+    def __init__(self, width,game_time=10):
         self.clock = pygame.time.Clock()
         self.time_left = game_time
-        self.font = pygame.font.Font(None, 36)
+        self.clock_format = ""
+        #calculate font size based on screen width
+        self.font = pygame.font.SysFont('monospace', int(width*0.045))
 
     def count_down(self):
         while self.time_left > 0:
-            self.time_left -= 1
+            self.time_left -=1
+            # Divide by 60 to get total minutes
+            minutes = self.time_left// 60
+            # Use modulus (remainder) to get seconds
+            seconds = self.time_left % 60
+            self.clock_format ="{0:02}:{1:02}".format(minutes, seconds)
             pygame.time.wait(1000)
+
         pygame.event.post(pygame.event.Event(pygame.USEREVENT))
         sys.exit()
 
     def draw_on(self, surface):
-        text = self.font.render(f"{self.time_left}", True, (0, 0, 0))
-        surface.blit(text, (10, 10))
+        text = self.font.render(self.clock_format, True,  (0, 0, 0))
+        surface.blit(text, (surface.get_width()/21, surface.get_height()/9))
 
 
 class Item(Drawable):
@@ -107,23 +168,47 @@ class Item(Drawable):
         self.surface.fill(color)
 
 
-class Game():
+class Bomb(Drawable):
+    def __init__(self, board, image_file, width=30, height=30):
+        self.width = width
+        self.height = height
+        self.x = random.randrange(int(board.surface.get_width() * 0.25),
+                                  int(board.surface.get_width() - self.width - board.surface.get_width() * 0.0484))
+        self.y = random.randrange(int(board.surface.get_height() * 0.04),
+                                  int(board.surface.get_height() - self.height - board.surface.get_height() * 0.032))
+
+        super().__init__(width, height, self.x, self.y)
+        self.load_image(image_file)
+
+    def load_image(self, image_file):
+        self.image = pygame.image.load(image_file)
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.surface.blit(self.image, (0, 0))
+
+
+class Game:
     def __init__(self, width, height):
         pygame.init()
         self.background = Background(
             'images/background.png',  width, height)
         self.board = Board(width, height, background=self.background)
-        self.hero1 = Hero(width=20, height=20, x=width/2, y=height/2)
+        self.hero1 = Hero(width=20, height=20, x=width/2, y=height/2, name="Player 1")
         self.hero2 = Hero(width=20, height=20, x=width//3,
-                          y=height//3, color=(255, 0, 0))
-        self.timer = Timer()
+                          y=height//3, color=(255, 0, 0), name="Player 2")
+        hearts1 = [Heart(width=width, height=height, live_type=True, player=1, number=i) for i in range(1, 4)]
+        hearts2 = [Heart(width=width, height=height, live_type=True, player=2, number=i) for i in range(1, 4)]
+        self.hero1.set_hearts(hearts1)
+        self.hero2.set_hearts(hearts2)
+        self.timer = Timer(width, 80)
         self.tab = []
+        self.bombs = []
 
     def run(self):
 
         threads = [
             th.Thread(target=self.timer.count_down),
             th.Thread(target=self.spawn_items),
+            th.Thread(target=self.spawn_bombs),
         ]
 
         # Run in separate threads
@@ -131,12 +216,16 @@ class Game():
             thread.start()
 
         while not self.handle_events():
+            self.bomb_colision()  # Sprawdzanie kolizji dla każdej bomby
+
+
             self.board.draw(
                 self.background,
                 self.hero1,
                 self.hero2,
                 self.timer,
                 *self.tab,
+                *self.bombs,
             )
 
         # Wait until both threads have finished
@@ -160,6 +249,8 @@ class Game():
             self.hero1.move(x=1, y=0, board=self.board)
         if key_input[pygame.K_DOWN]:
             self.hero1.move(x=0, y=1, board=self.board)
+        if key_input[pygame.K_b]:
+            self.hero1.remove_live()
 
         if key_input[pygame.K_a]:
             self.hero2.move(x=-1, y=0, board=self.board)
@@ -169,6 +260,24 @@ class Game():
             self.hero2.move(x=1, y=0, board=self.board)
         if key_input[pygame.K_s]:
             self.hero2.move(x=0, y=1, board=self.board)
+        if key_input[pygame.K_n]:
+            self.hero2.remove_live()
+
+    #check colision between hero and bombs
+    def bomb_colision(self):
+        for bomb in self.bombs:
+            if self.hero1.rect.colliderect(bomb.rect):
+                self.hero1.remove_live()
+                self.bombs.remove(bomb)
+                del bomb
+                break  # Dodajemy break, aby przerwać pętlę po znalezieniu kolizji
+
+        for bomb in self.bombs:
+            if self.hero2.rect.colliderect(bomb.rect):
+                self.hero2.remove_live()
+                self.bombs.remove(bomb)
+                del bomb
+                break  # Dodajemy break, aby przerwać pętlę po znalezieniu kolizji
 
     def spawn_items(self):
         while True:
@@ -176,7 +285,13 @@ class Game():
             self.tab.append(item)
             pygame.time.wait(10000)
 
+    def spawn_bombs(self):
+        while True:
+            bomb = Bomb(self.board, image_file='images/bomb.png')
+            self.bombs.append(bomb)
+            pygame.time.wait(10000)
+
 
 if __name__ == "__main__":
-    game = Game(800, 400)
+    game = Game(1200, 600)
     game.run()
