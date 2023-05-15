db7d2cbcc8a9c554a899347b6a481276d8873aae
Mimblaa <80205707+Mimblaa@users.noreply.github.com>
2023-05-15 02:42:34 +0200

    seperate code into different files

---
+++ board.py
@@ -0,0 +1,40 @@
+import pygame
+import pygame.locals
+from drawable import *
+import sys
+
+
+class Board:
+    def __init__(self, width, height, background=None):
+        self.surface = pygame.display.set_mode((width, height), 0, 32)
+        pygame.display.set_caption('Kaboom Combat')
+        self.background = background
+
+    def draw(self, *args):
+        if self.background:
+            self.surface.blit(self.background.image, (0, 0))
+        else:
+            self.surface.fill((255, 255, 255))
+
+        for arg in args:
+            if isinstance(arg, pygame.sprite.Group):
+                arg.draw(self.surface)
+            elif isinstance(arg, Hero):
+                arg.draw_on(self.surface)
+                for heart in arg.hearts:
+                    heart.draw_on(self.surface)
+            else:
+                arg.draw_on(self.surface)
+
+        pygame.display.update()
+
+
+class Background:
+    def __init__(self, image_file, width, height):
+        self.image = pygame.image.load(image_file)
+        self.width = width
+        self.height = height
+
+    def draw_on(self, surface):
+        scaled_image = pygame.transform.scale(self.image, (self.width, self.height))
+        surface.blit(scaled_image, (0, 0))
--- main.py
+++ main.py
@@ -5,224 +5,13 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 import random
 import sys
-import math
+from drawable import *
+from board import *
 
-
-class Board:
-    def __init__(self, width, height, background=None):
-        self.surface = pygame.display.set_mode((width, height), 0, 32)
-        pygame.display.set_caption('Kaboom Combat')
-        self.background = background
-
-    def draw(self, *args):
-        if self.background:
-            self.surface.blit(self.background.image, (0, 0))
-        else:
-            self.surface.fill((255, 255, 255))
-
-        for arg in args:
-            if isinstance(arg, pygame.sprite.Group):
-                arg.draw(self.surface)
-            elif isinstance(arg, Hero):
-                arg.draw_on(self.surface)
-                for heart in arg.hearts:
-                    heart.draw_on(self.surface)
-            else:
-                arg.draw_on(self.surface)
-
-        pygame.display.update()
-
-
-class Drawable:
-
-    def __init__(self, width, height, x, y, color=(0, 255, 0)):
-        self.width = width
-        self.height = height
-        self.color = color
-        self.surface = pygame.Surface(
-            [width, height], pygame.SRCALPHA, 32).convert_alpha()  # check it
-        self.rect = self.surface.get_rect(x=x, y=y)  # check it
-
-    def draw_on(self, surface):
-        surface.blit(self.surface, self.rect)
-
-
-class Hero(Drawable):
-    def __init__(self, width, height, x, y, color=(0, 255, 0), lives=3, name="Player"):
-        super().__init__(width, height, x, y, color)
-        self.surface.fill(color)
-        self.width = width
-        self.height = height
-        self.lives = lives
-        self.name = name
-        self.hearts = []
-
-    def set_hearts(self, hearts):
-        self.hearts = hearts
-        self.update_hearts()
-
-    def update_hearts(self):
-        for i, heart in enumerate(self.hearts):
-            if i < self.lives:
-                heart.live_type = True
-            else:
-                heart.live_type = False
-            heart.update_image()
-
-    def move(self, x, y, board):
-
-        if self.rect.x + x <= board.surface.get_width()*0.25 or \
-                self.rect.x + x >= board.surface.get_width() - self.width - board.surface.get_width()*0.0484:
-            x = 0
-
-        if self.rect.y + y <= board.surface.get_height()*0.04 or \
-                self.rect.y + y >= board.surface.get_height() - self.height-board.surface.get_height()*0.032:
-            y = 0
-
-        self.rect.x += x
-        self.rect.y += y
-
-    def remove_live(self):
-        if self.lives > 1:
-            self.lives -= 1
-        elif self.lives == 1:
-            self.lives -= 1
-            print("Player " + self.name + " is dead")
-        self.update_hearts()
-
-
-class Heart:
-    def __init__(self, width, height, live_type, number, player):
-        self.width = width
-        self.height = height
-        self.x_pos = width * 0.09 + (number - 1) * width * 0.029
-        self.y_pos = height * 0.52 + (player - 1) * height * 0.308
-        self.live_type = live_type
-        self.update_image()
-
-    def update_image(self):
-        if self.live_type:
-            image_file = 'images/full_heart.png'
-        else:
-            image_file = 'images/dead_heart.png'
-
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(
-            self.image, (int(self.width * 0.023), int(self.height * 0.035)))
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x_pos
-        self.rect.y = self.y_pos
-
-    def draw_on(self, surface):
-        surface.blit(self.image, self.rect)
-
-
-class Background:
-    def __init__(self, image_file, width, height):
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(
-            self.image, (width, height))  # resize image to fit window
-        self.rect = self.image.get_rect()
-        self.rect.left = 0
-        self.rect.top = 0
-
-    def draw_on(self, surface):
-        surface.blit(self.image, self.rect)
-
-
-class Timer():
-    def __init__(self, width,game_time=10):
-        self.clock = pygame.time.Clock()
-        self.time_left = game_time
-        self.clock_format = ""
-        #calculate font size based on screen width
-        self.font = pygame.font.SysFont('monospace', int(width*0.045))
-
-    def count_down(self):
-        while self.time_left > 0:
-            self.time_left -=1
-            # Divide by 60 to get total minutes
-            minutes = self.time_left// 60
-            # Use modulus (remainder) to get seconds
-            seconds = self.time_left % 60
-            self.clock_format ="{0:02}:{1:02}".format(minutes, seconds)
-            pygame.time.wait(1000)
-
-        pygame.event.post(pygame.event.Event(pygame.USEREVENT))
-        sys.exit()
-
-    def draw_on(self, surface):
-        text = self.font.render(self.clock_format, True,  (0, 0, 0))
-        surface.blit(text, (surface.get_width()/21, surface.get_height()/9))
-
-
-class Item(Drawable):
-    def __init__(self, board, width=10, height=10, color=(0, 0, 255), i=0, j=0):
-        self.width = width
-        self.height = height
-        rows = len(cord_list)
-        columns = len(cord_list[0])
-        self.x = math.ceil((board.surface.get_width() * 0.25) + math.ceil((board.surface.get_width()* 0.7 / rows) * i))
-        self.y = math.ceil((board.surface.get_height() * 0.04) + math.ceil((board.surface.get_height()* 0.9265 / columns) * j))
-
-        super().__init__(width, height, self.x, self.y, color)
-        self.surface.fill(color)
-
-
-class Bomb(Drawable):
-    def __init__(self, board, image_file, width=30, height=30, i=0, j=0):
-        self.width = width
-        self.height = height
-        rows = len(cord_list)
-        columns = len(cord_list[0])
-        print(rows)
-        print(columns)
-        self.x = math.ceil((board.surface.get_width() * 0.25) + math.ceil((board.surface.get_width() * 0.7 / rows) * i))
-        self.y = math.ceil(
-            (board.surface.get_height() * 0.04) + math.ceil((board.surface.get_height() * 0.9265 / columns) * j))
-        super().__init__(width, height, self.x, self.y)
-        self.load_image(image_file)
-
-    def load_image(self, image_file):
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        self.surface.blit(self.image, (0, 0))
-
-
-
-class Cube(Drawable):
-    def __init__(self, board, x, y, width=50, height=50, color=(255, 255, 0)):
-        super().__init__(width, height, x, y, color)
-        paths = ['images/cheese_brick.png', 'images/dirt_brick.png', 'images/sweet_brick.png', 'images/ice_cube.png']
-        image_file=paths[random.randrange(len(paths))]
-        self.load_image(image_file)
-
-    def check_collision(self, rect):
-        return self.rect.colliderect(rect)
-
-    def load_image(self, image_file):
-        self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        self.surface.blit(self.image, (0, 0))
-
-
-class Score:
-    def __init__(self, width, height):
-        self.score = 0
-        self.font = pygame.font.SysFont('monospace', int(width * 0.045))
-
-    def increase_score(self, points):
-        self.score += points
-
-    def reset_score(self):
-        self.score = 0
-
-    def draw_on(self, surface):
-        text = self.font.render("Score: " + str(self.score), True, (0, 0, 0))
-        surface.blit(text, (surface.get_width() / 1.5, surface.get_height() / 9))
 
 lock = th.Lock()
 cord_list = [[0 for j in range(20)] for i in range(16)]
+
 
 class Game:
 
@@ -231,8 +20,8 @@ class Game:
         self.background = Background(
             'images/background.png',  width, height)
         self.board = Board(width, height, background=self.background)
-        self.hero1 = Hero(width=20, height=20, x=width/2, y=height/2, name="Player 1")
-        self.hero2 = Hero(width=20, height=20, x=width//3, y=height//3, color=(255, 0, 0), name="Player 2")
+        self.hero1 = Hero(image_file='images/hero1.png',width=30, height=30, x=width/2, y=height/2, name="Player 1")
+        self.hero2 = Hero(image_file='images/hero2.png',width=30, height=30, x=width//3, y=height//3, color=(255, 0, 0), name="Player 2")
         hearts1 = [Heart(width=width, height=height, live_type=True, player=1, number=i) for i in range(1, 4)]
         hearts2 = [Heart(width=width, height=height, live_type=True, player=2, number=i) for i in range(1, 4)]
         self.hero1.set_hearts(hearts1)
@@ -241,8 +30,11 @@ class Game:
         self.tab = []
         self.bombs = []
         self.cubes = []
-        self.score1 = Score(width, height)
-        self.score2 = Score(width/2, height/2)
+        self.score1 = Score(width, height,1)
+        self.score2 = Score(width, height,2)
+        self.prof1 = Profile(width, height, 1)
+        self.prof2 = Profile(width, height, 2)
+
 
     def run(self):
 
@@ -270,7 +62,9 @@ class Game:
                 *self.bombs,
                 *self.cubes,
                 self.score1,
-                self.score2
+                self.score2,
+                self.prof1,
+                self.prof2,
             )
 
         # Wait until both threads have finished
@@ -344,7 +138,7 @@ class Game:
         global cord_list
         while True:
             i = random.randrange(len(cord_list))
-            j = random.randrange(len(cord_list[0]))
+            j = random.randrange(len(cord_list[0])-1)
             lock.acquire()
             if cord_list[i][j] == 0:
                 cord_list[i][j] = 1
@@ -361,7 +155,7 @@ class Game:
         global cord_list
         while True:
             i = random.randrange(len(cord_list))
-            j = random.randrange(len(cord_list[0]))
+            j = random.randrange(len(cord_list[0])-1)
             lock.acquire()
             if cord_list[i][j] == 0:
                 cord_list[i][j] = 1
@@ -377,23 +171,23 @@ class Game:
         global cord_list
         while True:
             i = random.randrange(len(cord_list))
-            j = random.randrange(len(cord_list[0]))
+            j = random.randrange(len(cord_list[0])-1)
             lock.acquire()
             if cord_list[i][j] == 0:
                 cord_list[i][j] = 1
                 rows = len(cord_list)
                 columns = len(cord_list[0])
                 x = math.ceil(
-                    (self.board.surface.get_width() * 0.25) + math.ceil((self.board.surface.get_width() * 0.7 / rows) * i))
+                    (self.board.surface.get_width() * 0.25) + math.ceil((self.board.surface.get_width() * 0.7 / columns) * i))
                 y = math.ceil((self.board.surface.get_height() * 0.04) + math.ceil(
-                    (self.board.surface.get_height() * 0.9265 / columns) * j))
-                width=math.floor((self.board.surface.get_width()* 0.7)/20)
+                    (self.board.surface.get_height() * 0.9265 / rows) * j))
+                width=math.floor((self.board.surface.get_width() * 0.7)/20)
                 height=math.floor((self.board.surface.get_height()*0.9265)/16)
-                cube = Cube(self.board, x, y, width, height)  # Tworzenie nowej kostki
-                self.cubes.append(cube)  # Dodanie kostki do listy
+                cube = Cube(self.board, x, y, width, height)
+                self.cubes.append(cube)
 
             lock.release()
-            pygame.time.wait(5000)  # Oczekiwanie przez 5 sekund przed wygenerowaniem kolejnej kostki
+            pygame.time.wait(5000)
 
     def check_collision(self, hero):
         for cube in self.cubes:
